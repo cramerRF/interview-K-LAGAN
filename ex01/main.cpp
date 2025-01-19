9e967da5670ccc11efef62e581180ea128da636d
CramerRF <victor.alonso.tr@gmail.com>
2025-01-19 20:50:17 +0100

    Ex01

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -1,7 +1,36 @@
 #include <iostream>
 
-int main()
+class Bombilla
 {
-    std::cout << "Hello, World!" << std::endl;
+public:
+    bool encendida = false;
+
+    int GetTemperatura()
+    {
+        if (encendida)
+        {
+            return 100;
+        }
+        else
+        {
+            return 0;
+        }
+    }
+};
+
+void EncenderBombilla(Bombilla &bombilla)
+{
+    bombilla.encendida = true;
+}
+
+int main(void)
+{
+    Bombilla bombilla;
+
+    std::cout << "Temperatura: " << bombilla.GetTemperatura() << std::endl;
+    std::cout << "Encendiendo bombilla..." << std::endl;
+    EncenderBombilla(bombilla);
+    std::cout << "Temperatura: " << bombilla.GetTemperatura() << std::endl;
+
     return 0;
 }
